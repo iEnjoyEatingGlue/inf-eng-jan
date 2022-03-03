7377c77d79962b87ef7fa1a7791376e335ff4c03
EmircanDogan23 <>
2022-03-03 16:13:18 +0100

    pls work

--- information-engineering-programing-jan/main.cpp
+++ information-engineering-programing-jan/main.cpp
@@ -5,6 +5,8 @@ using namespace std;
 int main()
 {
     cout << "asd" << endl;
+    cout << "pls work" << endl;
+    cout << "aasdasdsd" << endl;
     cout << "Hello World!" << endl;
     return 0;
 }
